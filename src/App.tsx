ddf87b27a8dbb1eed9124025468d16e6e331d3d3
Burhan <burhan221b@gmail.com>
2021-04-10 19:21:07 -0700

    HorizontalBarGraphCount

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import CircularRating from './components/CircularRating';
+import HorizontalBarGraphCount from './components/HorizontalBarGraphCount';
 
 function App() {
+  const test_bar_data = [{ count: 4, color: 'blue' }, { count: 10, color: 'green' }]
   return (
     <div className="App">
       <CircularRating rating={90} maxRating={100} />
+      <HorizontalBarGraphCount data={test_bar_data} />
     </div>
   );
 }
---
+++ src/components/HorizontalBarGraphCount.tsx
@@ -0,0 +1,79 @@
+import React, { CSSProperties, useEffect } from 'react';
+import '../styles/HorizontalBarGraphCount.css';
+
+
+export interface BarFeatures {
+    count: number,
+    color?: string,
+    index?: number
+}
+
+export interface BarProps {
+    count: number,
+    color?: string,
+    index: number
+}
+
+export interface HorizontalBarGraphCountProps {
+    data: BarFeatures[]
+}
+
+// Reference :: https://css-tricks.com/how-to-make-charts-with-svg/
+const HorizontalBarGraphCount: React.FunctionComponent<HorizontalBarGraphCountProps> = (props) => {
+    const { data } = props;
+    useEffect(() => {
+        var rect = document.querySelectorAll("rect");
+
+        function toggleComplete(rect: any) {
+            let i = 0;
+            for (let bar of rect) {
+                console.log(bar)
+                setTimeout(() => {
+                    const data_width = bar.getAttribute('data-width');
+                    console.log(data_width)
+                    bar.style.width = data_width;
+                }, 500 * i)
+                i++;
+            }
+            // if (rect) rect.forEach((a, i) => {
+            //     setTimeout(() => {
+            //         const data_width = a.getAttribute('data-width');
+            //         console.log(data_width)
+            //         a.style.width = data_width;
+            //     }, 500 * i)
+            // })
+        }
+        toggleComplete(rect)
+    }, [])
+    return (
+        <figure>
+            <figcaption>A graph that shows the number of fruit collected</figcaption>
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" className="chart"
+                width="420" height="150" aria-labelledby="title" role="img">
+                <title id="title">A bart chart showing information</title>
+                {data.map((plot, index) => {
+                    const { count, color } = plot
+                    return (<Bar key={index} count={count} color={color} index={index} />)
+                })}
+
+            </svg>
+        </figure>
+    );
+}
+
+export default HorizontalBarGraphCount;
+
+const Bar: React.FunctionComponent<BarProps> = (props) => {
+    const { count, color, index } = props;
+    const data_width = count * 10;
+    const rect_y = index * 20;
+    const text_x = data_width + 5;
+    const text_y = index === 0 ? 9.5 : 28 + (20 * (index - 1));
+    const rect_fill = color ? color : "#aaa"
+    return (
+        <g className="bar">
+            <rect className="setup" data-width={data_width} height="19" y={rect_y} fill={rect_fill}></rect>
+            <text x={text_x} y={text_y} dy=".35em">{count} apples</text>
+        </g>
+    )
+}
